f1c35636e9fa5aba232004be1dcfa2e1815cb73c
Joel Vasallo <joelvasallo@gmail.com>
2013-02-16 13:16:26 -0600

    Initialized NXTBT v2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nxtbt::Application.config.secret_token = '38f0cf497a5a5b0b07343c0a357a5691b05a0efeff73019f9ba58a730ce23570c0c9e66a402ae76278cc32bac8ef4c05cc931c924b5a51e43a690ea1966d99c2'
