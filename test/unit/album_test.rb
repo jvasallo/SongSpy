1d5b2dbf97584c3fa6d6003dfd92735ce048c98a
Joel Vasallo <joelvasallo@gmail.com>
2013-02-18 02:39:18 -0600

    Added some tests and fixtures

--- test/unit/album_test.rb
+++ test/unit/album_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Test ablility to find album record" do
+      a = Album.find_by_name("My Beautiful Dark Twisted Fantasy")
+      assert a.name == "My Beautiful Dark Twisted Fantasy", "Album name did not match successfully" 
+  end
+
+  test "Adding an artist to an album" do
+      album = Album.find_by_name("My Beautiful Dark Twisted Fantasy")
+      artist = Artist.find_by_name("Kanye West")
+      album.artist = artist
+      assert album.artist.length > 0
+  end
+
 end
