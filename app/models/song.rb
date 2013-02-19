c245b7ef48bafee8344d56c8e725237bdecfc175
Joel Vasallo <joelvasallo@gmail.com>
2013-02-18 18:41:34 -0600

    Forgot to include the artist_id

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  attr_accessible :album_id, :genre_id, :length, :lyrics, :name, :rating
+  attr_accessible :album_id, :genre_id, :length, :lyrics, :name, :rating, :artist_id
   
   belongs_to :artist 
   belongs_to :album
