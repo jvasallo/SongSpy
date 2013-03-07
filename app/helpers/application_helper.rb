2e3b6490b4fed89d79aded3f18fa123d75086daf
Joel Vasallo <joelvasallo@gmail.com>
2013-03-06 23:27:30 -0600

    Modified the db schema by incorporating extension

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
  def display_photo(item)
     if item.has_photo?
-      image_tag item.photo_path
+      image_tag item.photo_path, :width => "100px"
     else
        "No photo."
     end
